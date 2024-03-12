a2a37d025684bc030b1c39796a5faf8339897558
Alexey Skobkin <skobkin-ru@ya.ru>
2024-03-12 22:10:34 +0300

    Trying to fix #13. Adding logging to the middleware.

--- bot/middleware.go
+++ bot/middleware.go
@@ -3,14 +3,21 @@ package bot
 import (
 	"github.com/mymmrac/telego"
 	"github.com/mymmrac/telego/telegohandler"
+	"log/slog"
 )
 
 func (b *Bot) chatTypeStatsCounter(bot *telego.Bot, update telego.Update, next telegohandler.Handler) {
 	message := update.Message
 
 	if message == nil {
+		slog.Info("chat-type-middleware: update has no message. skipping.")
+
 		next(bot, update)
+
+		return
 	}
+
+	slog.Info("chat-type-middleware: counting message chat type in stats", "type", message.Chat.Type)
 
 	switch message.Chat.Type {
 	case telego.ChatTypeGroup, telego.ChatTypeSupergroup:
